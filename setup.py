f5d72da37f64a5092eb589fac70d5678bc8c50e5
Ola Brynildsrud <olbb@fhi.no>
2018-04-25 11:43:51 +0200

    Convert to package

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from distutils.util import convert_path
+
+versionholder = {}
+ver_path = convert_path('stx_subtyping/__init__.py')
+with open(ver_path) as init_file:
+    exec(init_file.read(), versionholder)
+
+setup(name='stx_subtyping',
+    version=versionholder['__version__'],
+    description='Subtyping of the stx genes of Escherichia coli using raw (FASTQ) data and contigs (FASTA).',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+    ],
+    keywords=['stx','Ecoli','Escherichia','coli','typing','shiga','shigatoxin','verotoxin'],
+    author=['Ola Brynildsrud','Philip Ashton'],
+    author_email='ola.brynildsrud@fhi.no',
+    license='MIT',
+    packages=['stx_subtyping'],
+    install_requires=[
+        'biopython',
+        'pysam'],
+    entry_points={
+        'console_scripts': ['stx_subtyping=stx_subtyping.stx_subtyping:main']
+    },
+    package_data={'emm_typing': ['LICENSE', 'setup.py','data/*']},
+    include_package_data=True
+)
